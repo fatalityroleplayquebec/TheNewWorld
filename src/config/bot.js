ec57e745ca76b651a85eeee2476f5a7513129bdc
fatalityroleplayquebec <fatalityroleplayquebec@gmail.com>
2026-05-29 12:13:07 -0400

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "fait par : Lynthox",
+        name: "Dev : The new world",
         // Activity type number (0 = Playing).
         type: 0, 
       },
